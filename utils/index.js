6b6c2b6175161102f8bbccfa9517a46b34f422cb
Ivan Spoljaric <i.spoljaric@reply.de>
2020-07-26 12:37:25 +0200

    Rewrite isNumber, re-build dist

--- utils/index.js
+++ utils/index.js
@@ -1,1 +1,17 @@
-export * from './is-valid';
+export function isValid(n) {
+  if (!Number.isFinite(n)) {
+    throw new TypeError('expected a number');
+  }
+
+  if (!Number.isInteger(n)) {
+    throw new Error('expected an integer');
+  }
+
+  if (!Number.isSafeInteger(n)) {
+    throw new Error('value above safe integer');
+  }
+
+  if (n <= 0) {
+    throw new Error('expected a number greater than 0');
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
   mode: 'production',
   entry: './src/index.js',
   output: {
+    globalObject: 'this',
     path: path.resolve(__dirname, 'dist'),
     filename: 'linear-array.js',
     library: 'linearArray',
